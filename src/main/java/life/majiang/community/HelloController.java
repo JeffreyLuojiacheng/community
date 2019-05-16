42fec5a0acf48959eefd477da04851bf5fd3353e
jchenluo <jchenluo@163.com>
2019-05-16 10:45:30 +0800

    创建项目

---
+++ src/main/java/life/majiang/community/HelloController.java
@@ -0,0 +1,12 @@
+package life.majiang.community;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/Hello")
+    public String say(){
+        return "hello!";
+    }
+}
